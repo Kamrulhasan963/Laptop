48630b9a2b6c9cf76c5b6886849b9e60691357e0
Kamrul Hasan <atmkhasan@gmail.com>
2017-11-16 12:27:14 -0400

    first commit

---
+++ src/main/java/com/Mybusiness/app/App.java
@@ -0,0 +1,13 @@
+package com.Mybusiness.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
